b4e9acf33e653ded58528cd0d131821c3f808b67
PxlPhile <pi@pxlphile>
2016-08-15 21:17:24 +0200

    Draw a 8x8 bitmap - first draft

--- draw-led.py
+++ draw-led.py
@@ -4,12 +4,43 @@ import max7219.led as led
 import time
 import sys
 
+# define x,y matrix resolution in pixel
+resolution = [8,8]
+maxX = resolution[0]
+maxY = resolution[1]
+device = None
+
+picture = [\
+	[0,0,0,0,0,0,0,0],\
+        [0,0,0,0,0,0,0,0],\
+        [0,0,0,0,0,0,0,0],\
+        [0,0,0,0,0,0,0,0],\
+        [0,0,0,0,0,0,0,0],\
+        [0,0,0,0,0,0,0,0],\
+        [0,0,0,0,0,0,0,0],\
+        [0,0,0,0,0,0,0,0]]
+
 def run():
-#	device = led.matrix(cascaded=1)
-	print "hello world"
-#	device.pixel(4, 4, 1, redraw=False)
-#	device.flush()
+	global device
+	device = led.matrix(cascaded=1)
+	drawBitmap()
 	time.sleep(10)
+
+def drawBitmap():
+	for currentY in range(maxY):
+		drawLine(currentY)
+	device.flush()
+
+def drawLine(currentY):
+	for currentX in range(maxX):
+		shouldDraw = resolveBitmapAt(currentX, currentY)
+		if shouldDraw:
+			device.pixel(currentX, currentY, 7, redraw=False)
+
+def resolveBitmapAt(currentX, currentY):
+	if picture[currentY][currentX] == 0:
+		return True
+	return False
 
 def main():
 	try:
